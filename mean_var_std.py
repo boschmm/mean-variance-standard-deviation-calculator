826893330a87beda480869788b70f7426c8c8efc
Mateo Bosch <91664431+boschmm@users.noreply.github.com>
2022-11-09 15:50:02 +0000

    Initial commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,35 @@
 import numpy as np
 
 def calculate(list):
+  if len(list) != 9:
+    raise ValueError ("List must contain nine numbers.")
+  else:
+    l = np.reshape(list, (3,3))
+    lf = l.flatten()
+    mc, vc, stdc, maxc, minc, sumc, mr, vr, stdr, maxr, minr, sumr = ([] for i in range(12))
+    for column in l.T:
+      mc.append(column.mean())
+      vc.append(column.var())
+      stdc.append(column.std())
+      maxc.append(column.max())
+      minc.append(column.min())
+      sumc.append(column.sum())
 
+    for row in l:
+      mr.append(row.mean())
+      vr.append(row.var())
+      stdr.append(row.std())
+      maxr.append(row.max())
+      minr.append(row.min())
+      sumr.append(row.sum())
+      
 
-
-
+    calculations = {
+      'mean': [mc, mr, lf.mean()],
+      'variance': [vc, vr, lf.var()],
+      'standard deviation': [stdc, stdr, lf.std()],
+      'max': [maxc, maxr, lf.max()],
+      'min': [minc, minr, lf.min()],
+      'sum': [sumc, sumr, lf.sum()]
+    }
     return calculations
